b1712642066d1e80e5aca8e7642405259364a290
1l3j1ndr0 <ja477236@uaeh.edu.mx>
2025-02-05 23:18:21 -0600

    Add files via upload

---
+++ Programa_1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+string revisar(string s) {
+	bool tieneNumero = false, tieneLetras = false;
+
+	for (int i = 0; i < s.length(); i++) {  // Uso de índices en lugar de range-based for
+		char c = s[i];
+		if (isdigit(c)) {
+			tieneNumero = true;
+		} else if (isalpha(c)) {
+			tieneLetras = true;
+		} else {
+			return "Contiene caracteres especiales";
+		}
+	}
+
+	if (tieneNumero && tieneLetras) {
+		return "Compuesta";
+	}
+	if (tieneNumero) {
+		return "Numero entero";
+	}
+	if (tieneLetras) {
+		return "Palabra";
+	}
+	return "Cadena vacía o no reconocida";
+}
+
+int main() {
+	string cadena;
+	int res=0;
+	while(res!=2) {
+		cout << "Ingresa una palabra o elemento: ";
+		cin >> cadena;
+		cout << "La cadena ingresada es: " << revisar(cadena) << endl<<endl;
+		cadena = "";
+
+		do {
+			cout <<endl<<endl<<"Quiere ingresar otra palabra? \n1)Si\n2)No" << endl;
+			cout <<endl<<"Respuesta: ";
+			cin>>res;
+		} while(res<1||res>2);
+	}
+}
